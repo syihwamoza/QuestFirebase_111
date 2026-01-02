23bf951376dca5659a3832a965332c0783d2d2fc
syihwamoza <mozakastella26@gmail.com>
2026-01-02 23:02:03 +0700

    menambahkan Application untuk inisialisasi container

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -11,3 +11,10 @@ class DefaultContainerApp : ContainerApp {
         FirebaseRepositorySiswa()
     }
 }
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
