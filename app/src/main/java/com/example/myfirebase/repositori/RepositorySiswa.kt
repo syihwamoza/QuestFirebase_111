96e1f76d0dae4be5e27655f742770b3ab130da19
syihwamoza <mozakastella26@gmail.com>
2026-01-02 23:05:54 +0700

    menambahkan fungsi simpan data siswa ke Firebase

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -28,4 +28,13 @@ class FirebaseRepositorySiswa : RepositorySiswa {
     }
 
 
-}
+override suspend fun postDataSiswa(siswa: Siswa) {
+    val docRef = if (siswa.id == 0L) collection.document() else collection.document(siswa.id.toString())
+    val data = hashMapOf(
+        "id" to (siswa.id.takeIf { it != 0L } ?: docRef.id.hashCode().toLong()), // Pastikan id konsisten
+        "nama" to siswa.nama,
+        "alamat" to siswa.alamat,
+        "telpon" to siswa.telpon
+    )
+    docRef.set(data).await()
+}}
