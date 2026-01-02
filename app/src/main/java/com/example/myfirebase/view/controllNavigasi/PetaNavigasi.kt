d5a49cfe3b1b80bfc1453bbc10df5c81ccdaea2c
syihwamoza <mozakastella26@gmail.com>
2026-01-02 23:07:23 +0700

    menambahkan entry composable aplikasi DataSiswa

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -19,23 +19,3 @@ Modifier){
     HostNavigasi(navController = navController)
 }
 
-@Composable
-fun HostNavigasi(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-){
-    NavHost(navController = navController, startDestination = DestinasiHome.route,
-        modifier = Modifier ){
-        composable(DestinasiHome.route) {
-            HomeScreen(navigateToItemEntry = { navController.navigate(
-                DestinasiEntry
-                    .route) },
-                navigateToItemUpdate = {
-                    navController.navigate("${DestinasiDetail.route}/${it}")})
-        }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
-            })
-        }
-    }
-}
