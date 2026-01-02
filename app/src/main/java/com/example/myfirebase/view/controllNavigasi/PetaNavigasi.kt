8732ccba8af8dc9d511868eba7dc4dd69827ddfe
syihwamoza <mozakastella26@gmail.com>
2026-01-02 23:08:16 +0700

    implementasi navigasi Compose pada aplikasi Data Siswa

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -18,4 +18,24 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(), mod
 Modifier){
     HostNavigasi(navController = navController)
 }
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(
+                DestinasiEntry
+                    .route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
+            })
+        }
+    }
+}
 
