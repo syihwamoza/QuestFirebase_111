98813b744a5eceb293b73c947ad427b44ea51062
syihwamoza <mozakastella26@gmail.com>
2026-01-02 22:47:06 +0700

    Membuat data class Siswa untuk penyimpanan data siswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -7,7 +7,6 @@ data class Siswa(
     val alamat: String = "",
     val telpon: String = ""
 )
-
 data class DetailSiswa(
     val id: Long = 0,
     val nama: String = "",
@@ -15,15 +14,3 @@ data class DetailSiswa(
     val telpon: String = ""
 )
 
-fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
-fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
-
-data class UIStateSiswa(
-    val detailSiswa: DetailSiswa = DetailSiswa(),
-    val isEntryValid: Boolean = false
-)
-
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
