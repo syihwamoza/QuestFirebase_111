093c9e36bb1c038484421b7aa7ef013b6b0b75ee
syihwamoza <mozakastella26@gmail.com>
2026-01-08 23:32:19 +0700

    Menambahkan layout Column di DetailDataSiswa untuk menampilkan field siswa

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -136,7 +136,21 @@ fun DetailDataSiswa(
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            BarisDetailData(
+                labelResID = R.string.nama1,
+                itemDetail = siswa!!.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )
+                )
+            )
 }
 
 
