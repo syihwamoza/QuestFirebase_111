86aff3331c8558bb6ce2db6785d36b1a876a98b7
syihwamoza <mozakastella26@gmail.com>
2026-01-08 23:31:36 +0700

    Menambahkan Composable DetailDataSiswa untuk menampilkan informasi siswa

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -126,6 +126,17 @@ private fun BodyDetailDataSiswa(
         )
     }
 }
+@Composable
+fun DetailDataSiswa(
+    siswa: Siswa?, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+
 }
 
 
