cf74dcad009b0e7ace30c0c831d496732d78351d
syihwamoza <mozakastella26@gmail.com>
2026-01-08 23:33:04 +0700

    Menambahkan field alamat dan telpon di DetailDataSiswa

--- app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -151,6 +151,29 @@ fun DetailDataSiswa(
                     )
                 )
             )
+            BarisDetailData(
+                labelResID = R.string.alamat1,
+                itemDetail = siswa.alamat,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )
+                )
+            )
+            BarisDetailData(
+                labelResID = R.string.telpon1,
+                itemDetail = siswa.telpon,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )
+                )
+            )
+        }
+    }
+}
+
+
 }
 
 
